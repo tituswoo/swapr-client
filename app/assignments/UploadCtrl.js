bcfee4f42a71e00d26eba41f0dfc84be6e4bf86b
unknown <vcnickerson@gmail.com>
2014-11-12 23:35:50 -0500

    Warning sign when invalid url, finished regex for youtube

--- app/assignments/UploadCtrl.js
+++ app/assignments/UploadCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by Vaughan on 10/27/2014.
  */
-angular.module('swapr').controller('UploadCtrl', function ($scope, $stateParams) {
+angular.module('swapr').controller('UploadCtrl', function ($scope, $stateParams, $http) {
     $scope.upload = {
         title: 'Lab 1 Video Upload',
         dueDate: '8/31/14',
@@ -11,7 +11,46 @@ angular.module('swapr').controller('UploadCtrl', function ($scope, $stateParams)
         id: $stateParams.id
     };
 
+    $scope.inputUrl = "";
     $scope.videoUrl = "";
+    $scope.urlIsValid=true;
+
+    $scope.checkUrl = function() {
+        console.log("Checking URL");
+        var inputUrlId = "";
+        //TODO: urls must currently be prefaced with "http://"
+        if ($scope.inputUrl.match(/youtube\.com\/watch\?v=([^\&\?\/]{11})/)) {
+            var index = $scope.inputUrl.indexOf("watch?v=") + 8;
+            inputUrlId = $scope.inputUrl.substring(index);
+        } else if ($scope.inputUrl.match(/youtube\.com\/embed\/([^\&\?\/]{11})/)) {
+            var index = $scope.inputUrl.indexOf("embed") + 6;
+            inputUrlId = $scope.inputUrl.substring(index);
+        } else if($scope.inputUrl.match(/youtu\.be\/([^\&\?\/]{11})/)){
+            var index = $scope.inputUrl.indexOf(".be") + 4;
+            inputUrlId = $scope.inputUrl.substring(index);
+        } else if($scope.inputUrl.match(/youtube\.com\/v\/([^\&\?\/]{11})/)){
+            console.log("youtube.com/v/")
+            var index = $scope.inputUrl.indexOf("/v/") + 3;
+            inputUrlId = $scope.inputUrl.substring(index);
+            console.log(inputUrlId);
+        }
+        $http.get('https://www.googleapis.com/youtube/v3/videos?id='+inputUrlId
+            +"&key=AIzaSyAaTtiCEM1194GMdnfNvA-aWFtaNTf_9UY&part=status").
+            success(function(data, status, headers, config) {
+                if(data.items.length < 1) {
+                    //TODO: display an error message to viewer
+                    $scope.urlIsValid=false;
+                    return;
+                } else if (data.items[0].status.privacyStatus == "public"
+                    || data.items[0].status.privacyStatus == "unlisted") {
+                    $scope.videoUrl = $scope.inputUrl;
+                    $scope.urlIsValid=true;
+                }
+            }).
+            error(function(data, status, headers, config) {
+                console.log("Error loading video url");
+            });
+    };
     //$scope.videoUrl = "https://www.youtube.com/watch?v=0Bmhjf0rKe8";
 
     $scope.intro = "Upload yer video here, matey! Arrgh...";
