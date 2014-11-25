cfe992a9048d4a73199b07d821cd2380b2de9dce
Titus Woo <titus.woo@me.com>
2014-11-25 13:39:56 -0500

    Restructured underlying video object.

--- app/gradeVideo/GradeVideoCtrl.js
+++ app/gradeVideo/GradeVideoCtrl.js
@@ -3,57 +3,57 @@
  */
 
 angular.module('swapr').controller('GradeVideoCtrl', function ($scope) {
-    $scope.videoUrl = "https://www.youtube.com/watch?v=0Bmhjf0rKe8";
-
-    $scope.onHover = function(value) {
-
+    $scope.peerReview = {
+        video: {
+            url: "https://www.youtube.com/watch?v=0Bmhjf0rKe8",
+            title: "Surprised Kitty"
+        },
+        criteria: [
+            {
+                title: 'Organizational Structure',
+                rating: {
+                    rate: 0,
+                    max: 5,
+                    readonly: false
+                },
+                feedback: 'enter feedback here.'
+            },
+            {
+                title: 'Content Models',
+                rating: {
+                    rate: 0,
+                    max: 5,
+                    readonly: false
+                },
+                feedback: 'enter feedback here.'
+            },
+            {
+                title: 'Content Prediction Discussion',
+                rating: {
+                    rate: 0,
+                    max: 5,
+                    readonly: false
+                },
+                feedback: 'enter feedback here.'
+            },
+            {
+                title: 'Content Overall',
+                rating: {
+                    rate: 0,
+                    max: 5,
+                    readonly: false
+                },
+                feedback: 'enter feedback here.'
+            },
+            {
+                title: 'Production Delivery',
+                rating: {
+                    rate: 0,
+                    max: 5,
+                    readonly: false
+                },
+                feedback: 'enter feedback here.'
+            }
+        ]
     };
-
-    $scope.criteria = [
-        {
-            title: 'Organizational Structure',
-            rating: {
-                rate: 0,
-                max: 5,
-                readonly: false
-            },
-            feedback: 'enter feedback here.'
-        },
-        {
-            title: 'Content Models',
-            rating: {
-                rate: 0,
-                max: 5,
-                readonly: false
-            },
-            feedback: 'enter feedback here.'
-        },
-        {
-            title: 'Content Prediction Discussion',
-            rating: {
-                rate: 0,
-                max: 5,
-                readonly: false
-            },
-            feedback: 'enter feedback here.'
-        },
-        {
-            title: 'Content Overall',
-            rating: {
-                rate: 0,
-                max: 5,
-                readonly: false
-            },
-            feedback: 'enter feedback here.'
-        },
-        {
-            title: 'Production Delivery',
-            rating: {
-                rate: 0,
-                max: 5,
-                readonly: false
-            },
-            feedback: 'enter feedback here.'
-        }
-    ];
 });
