54b8c12050f49db6f6556f3f3f6815db49cc2e2f
Titus Woo <titus.woo@me.com>
2015-04-09 14:46:57 -0400

    Got the scope working, fixed critical issue with nested views (ng-if vs ng-show).

--- app/courses/admin/CourseCtrl.js
+++ app/courses/admin/CourseCtrl.js
@@ -4,36 +4,35 @@
 
 angular.module('swapr.admin').controller('CourseCtrl', ['$scope', '$stateParams', 'Courses', 'Assignments', 'Users',
     function ($scope, $stateParams, Courses, Assignments, Users) {
-    Courses.getCourse($stateParams.id).then(function (course) {
-        $scope.course = course;
-    });
-
-    Assignments.getAssignments($stateParams.id).then(function (assignments) {
-        $scope.assignments = assignments;
-    });
-
-    $scope.editAssignment = function (a) {
-        $scope.currentAssignment = a;
-        $scope.changeView(a);
-    };
-
-    $scope.loadStudents = function () {
-        // @todo: add option to filter user type. Assume they're all students for now.
-        Users.getUsers($stateParams.id).then(function (users) {
-            $scope.students = users;
+        Courses.getCourse($stateParams.id).then(function (course) {
+            $scope.course = course;
         });
-    };
 
-    $scope.curView = 'tabView@courses.course';
-    $scope.changeView = function (a) {
-        if (a.type === 'evaluation') {
-            $scope.curView = 'peerReview@courses.course';
-        } else if (a.type === 'submission') {
-            $scope.curView = 'videoUpload@courses.course';
-        }
+        Assignments.getAssignments($stateParams.id).then(function (assignments) {
+            $scope.assignments = assignments;
+        });
+
+        $scope.editAssignment = function (a) {
+            $scope.changeView(a);
+        };
+
+        $scope.loadStudents = function () {
+            // @todo: add option to filter user type. Assume they're all students for now.
+            Users.getUsers($stateParams.id).then(function (users) {
+                $scope.students = users;
+            });
+        };
+
+        $scope.curView = 'tabView@courses.course';
+        $scope.changeView = function (a) {
             $scope.curCourse = a;
-    };
-    $scope.parentControllerTest = function () {
-        alert("Parent controller works");
-    };
-}]);
+            if (a.type === 'evaluation') {
+                $scope.curView = 'peerReview@courses.course';
+            } else if (a.type === 'submission') {
+                $scope.curView = 'videoUpload@courses.course';
+            }
+        };
+        $scope.parentControllerTest = function () {
+            alert("Parent controller works");
+        };
+    }]);
--- app/courses/admin/VideoUploadCtrl.js
+++ app/courses/admin/VideoUploadCtrl.js
@@ -1,4 +1,6 @@
-angular.module('swapr.admin').controller('VideoUploadCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
+angular.module('swapr.admin').controller('VideoUploadCtrl', ['$scope', '$stateParams', function ($scope) {
     // @todo: get param id to load in the data for the assignment.
-    console.log($scope.currentAssignment);
+    $scope.curCourse = $scope.$parent.curCourse;
+
+    console.log($scope.curCourse);
 }]);
